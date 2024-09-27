ba5665cedcdab882aedb0c21ced7f0c0128e59b8
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2024-09-27 18:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (96.55%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,22 +1,14 @@
 class Solution {
     public static int[] plusOne(int[] digits) {
         int n = digits.length;
-
-        // Loop through the array from right to left
         for (int i = n - 1; i >= 0; i--) {
-            // Check if the current digit is less than 9
             if (digits[i] < 9) {
-                // Increment the digit by 1
                 digits[i]++;
                 return digits;
             } else {
-                // Set the current digit to 0
                 digits[i] = 0;
             }
         }
-
-        // If we're here, it means every element was 9
-        // Add 1 at the beginning of the array
         int[] result = new int[n + 1];
         result[0] = 1;
 
